a166da935c058cf9e25adaf5371463993eee4c90
xblanc33 <blancxav@gmail.com>
2019-07-09 18:43:58 +0200

    more tests

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const Ngram = require('./Ngram.js');
 const NgramSuccessorModel = require('./NgramSuccessorModel.js');
 const Sequence = require('./Sequence.js');
 const SequenceSuite = require('./SequenceSuite.js');
+const NaturalnessDynamicModel = require('./NaturalnessDynamicModel.js');
 
 module.exports.Event = Event;
 module.exports.NaturalnessModel = NaturalnessModel;
+module.exports.NaturalnessDynamicModel = NaturalnessDynamicModel;
 module.exports.Ngram = Ngram;
 module.exports.NgramSuccessorModel = NgramSuccessorModel;
 module.exports.Sequence = Sequence;
