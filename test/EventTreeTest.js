5c841e1ebac4fe85205ee72aecca300013e14532
xblanc33 <blancxav@gmail.com>
2019-07-10 14:19:53 +0200

    undefined final

--- test/EventTreeTest.js
+++ test/EventTreeTest.js
@@ -126,7 +126,7 @@ describe('EventTree', function () {
             assert.equal(map.get(eventA).length, 2);
         })
 
-        it('should return probability for a not final event', () => {
+        it('should return undefined for a not final event', () => {
             let eventA = new Event('a');
             let eventB = new Event('b');
             let aabaList = [eventA, eventA, eventB, eventA];
@@ -140,31 +140,7 @@ describe('EventTree', function () {
             let map = tree.getProbabilityMap([eventA, eventA]);
 
             //p(b | aa)
-            assert.equal(map.get(eventB)[0], 1/3); 
-
-            let abaaList = [eventA, eventB, eventA, eventA];
-            tree.learnAllSuffix(abaaList);
-            map = tree.getProbabilityMap([eventA]);
-
-            assert.equal(map.get(eventB)[0], 1/5); 
-        })
-
-        it('should return probability form empty context', () => {
-            let eventA = new Event('a');
-            let eventB = new Event('b');
-            let aabaList = [eventA, eventA, eventB, eventA];
-            let aaaaList = [eventB, eventA, eventA, eventA];
-
-            let tree = new EventTree(4);
-            tree.learnAllSuffix(aabaList);
-            tree.learnAllSuffix(aaaaList);
-
-            //p(? | aa) and p(? | a)
-            let map = tree.getProbabilityMap([]);
-
-            //p(b | aa)
-            assert.equal(map.get(eventA)[0], 6/8); 
-            assert.equal(map.get(eventB)[0], 2/8); 
+            assert.equal(map.get(eventB), undefined); 
         })
     });
 });
